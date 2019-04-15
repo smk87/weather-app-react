0765177b343df60d709a70e12b42e9d237fb6d3d
smk87 <30999133+smk87@users.noreply.github.com>
2019-04-15 19:12:29 +0600

    Build update.

---
+++ build/precache-manifest.f9a3c1286d6b2757d8787df697086d98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f0b97747d843d0f33259",
+    "url": "/static/js/main.f0b97747.chunk.js"
+  },
+  {
+    "revision": "06c32d0d07fde1999f3a",
+    "url": "/static/js/1.06c32d0d.chunk.js"
+  },
+  {
+    "revision": "f0b97747d843d0f33259",
+    "url": "/static/css/main.c8ec3e0c.chunk.css"
+  },
+  {
+    "revision": "24bb9789224587e057c3c3150439106d",
+    "url": "/index.html"
+  }
+];
